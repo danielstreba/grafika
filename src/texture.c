a086d905e9c858f8f66bab4cce8b8595185ebe11
danielstreba <danielstreba@gmail.com>
2020-05-08 18:38:50 +0200

    Fix black lines in the edge of skybox

--- src/texture.c
+++ src/texture.c
@@ -4,5 +4,9 @@
 
 GLuint load_texture(char *filename)
 {
+  // Fixes most of black lines in the edge of skybox
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
   return SOIL_load_OGL_texture(filename, SOIL_LOAD_RGB, 0, 0);
 }
